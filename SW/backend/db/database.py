66b5247c233a3a8f3d7d2980fe204ab86a1c0f52
Mona <mona.22@gmail.com>
2026-01-17 00:34:32 +0200

    db deployemnt

--- SW/backend/db/database.py
+++ SW/backend/db/database.py
@@ -7,7 +7,8 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise RuntimeError("❌ DATABASE_URL is not set in .env")
+    raise RuntimeError("❌ DATABASE_URL is not set")
+
 
 engine = create_engine(
     DATABASE_URL,
