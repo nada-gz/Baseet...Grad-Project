683a6fa6d9136ed20638c51182ccb7b5a5111bda
Nada <mo@Nadas-MacBook-Air-2.local>
2026-01-16 22:20:00 +0200

    Connected the current lesson with its materials and assignment

--- SW/backend/schemas/lesson_schema.py
+++ SW/backend/schemas/lesson_schema.py
@@ -1,5 +1,6 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
+from schemas.material_schema import MaterialRead
 
 
 class LessonRead(BaseModel):
@@ -16,6 +17,7 @@ class LessonRead(BaseModel):
     status: str
 
     number: str
+    materials: List[MaterialRead] = []
 
     class Config:
         orm_mode = True
