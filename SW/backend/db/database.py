3e33a24be227cf541af30c31dbb1696b99a84522
Mona <mona.22@gmail.com>
2026-01-17 03:05:35 +0200

    Fix Railway DATABASE_URL crash

--- SW/backend/db/database.py
+++ SW/backend/db/database.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    raise RuntimeError("❌ DATABASE_URL is not set")
+#if not DATABASE_URL:
+    #raise RuntimeError("❌ DATABASE_URL is not set")
 
 
 engine = create_engine(
