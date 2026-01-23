7b4f46fe76c3d0267e58cdf95bf268ace8438b27
Mona <mona.22@gmail.com>
2026-01-23 21:24:22 +0200

    update env

--- SW/frontend/src/services/api.js
+++ SW/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',// it was 'http://127.0.0.1:8000', i changed it for deployment 
+  baseURL: import.meta.env.VITE_API_URL,// it was 'http://127.0.0.1:8000', i changed it for deployment 
   headers: { 'Content-Type': 'application/json' },
 });
 
