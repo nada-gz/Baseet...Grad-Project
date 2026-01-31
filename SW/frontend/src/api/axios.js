bf7d88ff194f940b64fa5aa9c9cedb8210eded5f
Mona <mona.22@gmail.com>
2026-01-31 21:05:46 +0200

    update react url

--- SW/frontend/src/api/axios.js
+++ SW/frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env?.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 if (!API_URL) {
   console.error("❌ REACT_APP_API_URL is missing");
