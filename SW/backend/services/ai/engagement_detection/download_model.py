3d9ef3fea03e8430f12ba68a21aec65e30d32619
Hana Hatem <h.hatem2262@nu.edu.eg>
2026-06-19 22:26:07 +0000

    changed the model

--- SW/backend/services/ai/engagement_detection/download_model.py
+++ SW/backend/services/ai/engagement_detection/download_model.py
@@ -1,13 +1,15 @@
-from transformers import pipeline
+from transformers import BeitForImageClassification, AutoImageProcessor
 from pathlib import Path
 
-MODEL_ID = "gowdaman/gowdaman-emotion-detection"
+MODEL_ID = "nihar245/Expression-Detection-BEIT-Large"
 LOCAL_DIR = Path(__file__).resolve().parent / "model"
 
 def main():
     print("Downloading model...")
-    clf = pipeline("image-classification", model=MODEL_ID)
-    clf.save_pretrained(str(LOCAL_DIR))
+    model = BeitForImageClassification.from_pretrained(MODEL_ID)
+    processor = AutoImageProcessor.from_pretrained(MODEL_ID)
+    model.save_pretrained(str(LOCAL_DIR))
+    processor.save_pretrained(str(LOCAL_DIR))
     print(f"Model saved to: {LOCAL_DIR}")
 
 if __name__ == "__main__":
